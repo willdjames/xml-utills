202d39ce2cd244bcbd3c439c6803f22e571c24d6
Wiiliam D C Santana <william.santana@provider-it.com.br>
2018-11-17 15:38:17 -0200

    Prepara a classe para ler xml

---
+++ src/br/leitura/LeitorXml.java
@@ -0,0 +1,7 @@
+package br.leitura;
+
+import org.xml.sax.helpers.DefaultHandler;
+
+public class LeitorXml extends DefaultHandler {
+
+}
---
+++ src/br/leitura/MainLeitorXml2.java
@@ -0,0 +1,37 @@
+package br.leitura;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.XMLReaderFactory;
+
+public class MainLeitorXml2 {
+	
+	public static void main(String[] args) throws SAXException, IOException {
+		
+//		Prepara o source para leitura
+		InputStream byteStream = new FileInputStream("src/venda.xml");
+		InputSource input = new InputSource(byteStream);
+		
+//		Fabrica para ler o arquivo
+		XMLReader reader = XMLReaderFactory.createXMLReader();
+
+//		Instancia a logica do leitor
+		LeitorXml handler = new LeitorXml();
+
+//		Le o conteudo
+		reader.setContentHandler(handler);
+
+//		Recebe o arquivo source
+		reader.parse(input);
+		
+		
+		
+		
+	}
+
+}
